8d1fb8eb4db18031fff3c590c10bfa6f3b8306bc
babar <babbark32@gmail.com>
2022-10-18 20:56:43 +0530

    responding select menus

--- 8-select-menus/events/selectMenuInteraction.js
+++ 8-select-menus/events/selectMenuInteraction.js
@@ -1,7 +1,23 @@
+const wait = require('node:timers/promises').setTimeout
+
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
     if (!interaction.isSelectMenu()) return
-    console.log(interaction)
+    // console.log(interaction)
+
+    if (interaction.customId === 'select') {
+      //   await interaction.update({
+      //     content: 'Something was selected!',
+      //     components: [],
+      //   })
+
+      await interaction.deferUpdate()
+      await wait(4000)
+      await interaction.editReply({
+        content: 'Something was selected!',
+        components: [],
+      })
+    }
   },
 }
