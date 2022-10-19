7e14d38bf227e5bff9d2a680873973338d4cd434
babar <babbark32@gmail.com>
2022-10-19 18:23:03 +0530

    reorganising

--- 6-slash-commands/events/interactionCreate.js
+++ 6-slash-commands/events/interactionCreate.js
@@ -1,9 +1,6 @@
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
-    console.log(
-      `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
-    )
     if (!interaction.isChatInputCommand()) return
 
     const command = interaction.client.commands.get(interaction.commandName)
